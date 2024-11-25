de51f4acf6c5d0826c136a3104301b6edf74bfad
liuchiyun <m15879038981@163.com>
2024-11-25 09:30:53 +0800

    add

--- cmd/docker/main.go
+++ cmd/docker/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
 )
 
 func main() {
-  fmt.Println("hello")
+	fmt.Println("hello")
+}
+
+func testBaseCmd() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+	}
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+	cmd.Stdin = os.Std
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run; err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
 }
